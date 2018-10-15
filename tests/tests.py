2926ebd7fc9b003c99257c9d715d573458db9acb
Jully Anne Motta <jmotta@jmotta.com>
2018-10-15 18:47:20 -0300

    testes na api

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,9 @@
 import unittest, requests
 
 def test_img_api():
-    response = requests.get("http://placekitten.com/510/210")
-    assert response.status_code == 200 
+    resp = requests.get("http://placekitten.com/510/210")
+    assert resp.status_code == 200 
 
 def test_fact_api():
-    responde = requests.get("https://catfact.ninja/fact")
-    assert response.status_code == 200
+    resp = requests.get("https://catfact.ninja/fact")
+    assert resp.status_code == 200
